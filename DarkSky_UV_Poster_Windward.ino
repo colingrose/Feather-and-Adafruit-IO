e2e51b8d5041747788fd6a23da4757abbcd3c94d
colingrose <47397880+colingrose@users.noreply.github.com>
2022-04-10 13:33:20 -0700

    Update DarkSky_UV_Poster_Windward.ino

--- DarkSky_UV_Poster_Windward.ino
+++ DarkSky_UV_Poster_Windward.ino
@@ -16,32 +16,18 @@
 #endif
 
 // pin used to control the servo
-#define SERVO_PIN 2
+#define SERVOAZIMUTH_PIN 2
+#define SERVOALTITUDE_PIN 3
 
-// create an instance of the servo class
-Servo servo;
+// create two instances of the servo class
+Servo servoAzimuth;
+Servo servoAltitude;
 
-// set up the 'servo' feed
-AdafruitIO_Feed *servo_feed = io.feed("uv-index.marvista");
-
-// External LED for UV Alert. Hook up your light to pin 14. Without a resister it's very bright, but ok.
-#define UValert       14
-boolean indicator = false;
-
-// variables for the blinking light
-int ledState = LOW; //ledState used to set the LED
-// Generally, you should use "unsigned long" for variables that hold time
-// The value will quickly become too large for an int to store
-unsigned long previousMillis = 0;        // will store last time LED was updated
-// constants won't change:
-long interval = 500;           // interval at which to blink (milliseconds)
-
-int lastValue;
+// set up the 'servo' feeds
+AdafruitIO_Feed *servoAzimuth_feed = io.feed("sun-position.azimuth");
+AdafruitIO_Feed *servoAltitude_feed = io.feed("sun-position.altitude");
 
 void setup() {
-
-  pinMode(UValert, OUTPUT);
-  pinMode(13, OUTPUT); //to include a reset button, hook one lead to RST, one lead to pin 13 (ground)
 
   // start the serial connection
   Serial.begin(115200);
@@ -50,38 +36,44 @@ void setup() {
   while (! Serial);
 
   // tell the servo class which pin we are using
-  servo.attach(SERVO_PIN);
-  servo.write(0);
+  servoAzimuth.attach(SERVOAZIMUTH_PIN);
+  servoAltitude.attach(SERVOALTITUDE_PIN);
+  servoAzimuth.write(0);
   delay(1000);
-  servo.write(180);
+  servoAzimuth.write(359);
   delay(1000);
-  servo.write(0);
+  servoAzimuth.write(0);
+  delay(1000);
+  servoAltitude.write(0);
+  delay(1000);
+  servoAltitude.write(90);
+  delay(1000);
+  servoAltitude.write(0);
+  delay(1000);
 
 
   // connect to io.adafruit.com
   Serial.print("Connecting to Adafruit IO");
   io.connect();
 
-  // set up a message handler for the 'servo' feed.
+  // set up a message handler for the 'servo' feeds.
   // the handleMessage function (defined below)
   // will be called whenever a message is
   // received from adafruit io.
-  servo_feed->onMessage(handleMessage);
+  servoAzimuth_feed->onMessage(handleMessage);
+  servoAltitude_feed->onMessage(handleMessage);
 
   // wait for a connection
   while (io.status() < AIO_CONNECTED) {
     //Serial.print(".");
     Serial.println(io.statusText());  //troubleshooting the endless dots when connecting...........
-    digitalWrite(UValert, HIGH);
-    delay(50);
-    digitalWrite(UValert, LOW);
-    delay(50);
   }
 
   // we are connected
   Serial.println();
   Serial.println(io.statusText());
-  servo_feed->get();
+  servoAzimuth_feed->get();
+  servoAltitude_feed->get();
 
 }
 
@@ -99,57 +91,35 @@ void loop() {
     digitalWrite(UValert, LOW);
   }
 }
-// this function is called whenever a 'servo' message
+// this function is called whenever an 'Azimuth' message
 // is received from Adafruit IO. it was attached to
-// the servo feed in the setup() function above.
-void handleMessage(AdafruitIO_Data *data) {
+// the azimuth servo feed in the setup() function above.
+void handleMessage(AdafruitIO_Data *servoAzimuth_feed) {
 
   // convert the data to integer
-  int uvIndex = data->toInt();
+  int azimuth = data->toInt();
 
-  //print the UV Index
-  Serial.print("UV index is ");
-  Serial.println(uvIndex);
+  //print the Azimuth
+  Serial.print("Azimuth is ");
+  Serial.println(azimuth);
 
-  // make sure we don't exceed the limit
-  // of the servo. the range is from 0
-  // to 11.
-  if (uvIndex < 0)
-    uvIndex = 0;
-  else if (uvIndex > 11)
-    uvIndex = 11;
-  //turn on the UV alert light if the UV value is 6 or higher
-  if (uvIndex >= 6) {
-    indicator = true;
-  }
-  else {
-    indicator = false;
-  }
-  if (lastValue != uvIndex) {
-    //map the uvIndex from 0-11 to an angle between 180-0
-    int uvIndexPosition = map(uvIndex, 0, 11, 170, 0);
-
-    //set the servo to the correct angle
-    servo.write(uvIndexPosition);
-    lastValue = uvIndex;
+  //set the servo to the correct angle
+  servoAzimuth.write(azimuth);
   }
 }
+// this function is called whenever an 'Altitude' message
+// is received from Adafruit IO. it was attached to
+// the altitude servo feed in the setup() function above.
+void handleMessage(AdafruitIO_Data *servoAltitude_feed) {
 
-void blink() {
-  unsigned long currentMillis = millis();
+  // convert the data to integer
+  int altitude = data->toInt();
 
-  if (currentMillis - previousMillis >= interval) {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;
+  //print the Azimuth
+  Serial.print("Altitude is ");
+  Serial.println(altitude);
 
-    // if the LED is off turn it on and vice-versa:
-    if (ledState == LOW) {
-      ledState = HIGH;
-    } else {
-      ledState = LOW;
-    }
-
-    // set the LED with the ledState of the variable:
-    digitalWrite(UValert, ledState);
+  //set the servo to the correct angle
+  servoAltitude.write(altitude);
   }
 }
