addcbe2b64219676abdae33f8ff362ded463d2da
colingrose <47397880+colingrose@users.noreply.github.com>
2022-04-14 10:43:25 -0700

    Update sunTracker.ino

--- sunTracker.ino
+++ sunTracker.ino
@@ -24,39 +24,20 @@ Servo servoAZ;
 Servo servoAL;
 
 // set up the 'servo' feed
-AdafruitIO_Feed *servo_feedAZ = io.feed("sun-position.azimuth");
-AdafruitIO_Feed *servo_feedAL = io.feed("sun-position.altitude");
-
-// External LED for UV Alert. Hook up your light to pin 14. Without a resister it's very bright, but ok.
-#define alert       13
-boolean indicator = false;
-
-// variables for the blinking light
-int ledState = LOW; //ledState used to set the LED
-// Generally, you should use "unsigned long" for variables that hold time
-// The value will quickly become too large for an int to store
-unsigned long previousMillis = 0;        // will store last time LED was updated
-// constants won't change:
-long interval = 500;           // interval at which to blink (milliseconds)
-
-int lastAZValue;
-int lastALValue;
+AdafruitIO_Feed *feedAZ = io.feed("sun-position.azimuth");
+AdafruitIO_Feed *feedAL = io.feed("sun-position.altitude");
 
 void setup() {
 
-  pinMode(alert, OUTPUT);
-  pinMode(13, OUTPUT); //to include a reset button, hook one lead to RST, one lead to pin 13 (ground)
-
   // start the serial connection
   Serial.begin(115200);
+  Serial.println("Hello!");
 
   // wait for serial monitor to open
   while (! Serial);
 
-  // tell the servo class which pin we are using
+  // tell the servo class which pin we are using and test
   servoAZ.attach(SERVOAZ_PIN);
-  servoAL.attach(SERVOAL_PIN);
-
   //test the servos
   servoAZ.write(0);
   delay(1000);
@@ -64,49 +45,52 @@ void setup() {
   delay(1000);
   servoAZ.write(0);
   delay(1000);
-  servoAL.write(0);
-  delay(1000);
-  servoAL.write(180);
-  delay(1000);
-  servoAL.write(0);
+  servoAL.detach();
 
+  
+  servoAL.attach(SERVOAL_PIN);
+  servoAL.write(90);
+  delay(1000);
+  servoAL.write(150);
+  delay(1000);
+  servoAL.write(90);
+  delay(1000);
 
   // connect to io.adafruit.com
-  Serial.print("Connecting to Adafruit IO");
+  Serial.println("Connecting to Adafruit IO");
   io.connect();
 
   // set up a message handler for the 'servo' feeds.
   // the handleMessage functions (defined below)
   // will be called whenever a message is
   // received from adafruit io.
-  servo_feedAZ->onMessage(handleMessageAZ);
-  servo_feedAL->onMessage(handleMessageAL);
+  feedAZ->onMessage(handleMessageAZ);
+  feedAL->onMessage(handleMessageAL);
 
   // wait for a connection
   while (io.status() < AIO_CONNECTED) {
-    //Serial.print(".");
-    Serial.println(io.statusText());  //troubleshooting the endless dots when connecting...........
-    digitalWrite(alert, HIGH);
-    delay(50);
-    digitalWrite(alert, LOW);
-    delay(50);
+    Serial.print(".");
+    //Serial.println(io.statusText());  //troubleshooting the endless dots when connecting...........
+    delay(100);
   }
 
   // we are connected
-  Serial.println();
   Serial.println(io.statusText());
-  servo_feedAZ->get();
-  servo_feedAL->get();
+
+  //get data from feeds
+  feedAZ->get();
+  feedAL->get();
 
 }
 
 void loop() {
-
   // io.run(); is required for all sketches.
   // it should always be present at the top of your loop
   // function. it keeps the client connected to
   // io.adafruit.com, and processes any incoming data.
+
   io.run();
+
 }
 
 
@@ -124,40 +108,50 @@ void handleMessageAZ(AdafruitIO_Data *data) {
   else if (Azimuth > 270)
     Azimuth = 270;
 
-  if (lastAZValue != Azimuth) {
-    //map the azimuth from east to west (daytime) to 0-180 on the servo
-    int AZServoPosition = map(Azimuth, 90, 270, 180, 0);
+  //map the azimuth from east to west (daytime) to 0-180 on the servo
+  int AZServoPosition = map(Azimuth, 90, 270, 180, 0);
 
-    //set the servo to the correct angle
-    servoAZ.write(AZServoPosition);
-    lastAZValue = Azimuth;
+  //set the servo to the correct angle
 
-    //print the Azimuth
-    Serial.print("Azimuth is ");
-    Serial.print(Azimuth);
-    Serial.print(" (servo position ");
-    Serial.print(AZServoPosition);
-    Serial.print(")");
-  }
+  servoAZ.write(AZServoPosition);
+  delay(1000);
+
+  //print the Azimuth
+  Serial.print("Azimuth is ");
+  Serial.print(Azimuth);
+  Serial.print(" (servo position ");
+  Serial.print(AZServoPosition);
+  Serial.println(")");
 }
 
 
 // this function is called whenever an 'altitude' message
 // is received from Adafruit IO. it was attached to
 // the servo feed in the setup() function above.
-void handleMessageAL(AdafruitIO_Data *data) {
+void handleMessageAL(AdafruitIO_Data *data2) {
 
   // convert the data to integer
-  int Altitude = data->toInt();
+  int Altitude = data2->toInt();
+  
+   //print the Altitude
+  Serial.print("Altitude is ");
+  Serial.print(Altitude);
+  
+// make sure the altitude never drops below 15 (because servo gets stuck)
 
-  //print the Altitude
-  Serial.print(" and Altitude is ");
-  Serial.println(Altitude);
 
-  if (lastALValue != Altitude) {
+  //map the Altitude from 0-90
+  int ALServoPosition = map(Altitude, 0, 90, 90, 180);
 
-    //set the servo to the correct angle
-    servoAL.write(Altitude);
-    lastALValue = Altitude;
-  }
+    if (ALServoPosition > 185)
+    ALServoPosition = 165;
+
+  //set the servo to the correct angle
+  servoAL.write(ALServoPosition);
+  delay(1000);
+
+  //print the servo position
+  Serial.print(" (servo position ");
+  Serial.print(ALServoPosition);
+  Serial.println(")");
 }
